1b388351ad77ca7dc6db04f09d0d3f4e99bad0ff
charul1995 <charu1234patidar@gmail.com>
2019-04-11 10:31:53 +0530

    1 file modified

--- src/app/feed-detail/feed-detail.component.ts
+++ src/app/feed-detail/feed-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FromService } from '../formSubmit.service';
 
 @Component({
   selector: 'app-feed-detail',
@@ -6,19 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed-detail.component.css']
 })
 export class FeedDetailComponent implements OnInit {
-
-  constructor() { }
+  feed : any;
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private  formService: FromService) { }
 
   ngOnInit() {
-    var slug=this.activatedRoute.snapshot.url[1].path?this.activatedRoute.snapshot.url[1].path:'';
-     this.feedService.getFeedBySlug(slug).subscribe( responsedatas =>
-          {
-                   this.feed=responsedatas.data;  
-                   console.log(responsedatas)   ;         
-             }
-      );
+    var slug = this.activatedRoute.snapshot.url[1].path ? this.activatedRoute.snapshot.url[1].path : '';
+    this.formService.getFeedBySlug(slug).subscribe(responsedatas => {
+      this.feed = responsedatas.data;
+      console.log(responsedatas);
+    }
+    );
 
   }
-  }
-
 }
